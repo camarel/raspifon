bd9dffa4c6986ce8f0556cde645a0e12290b011e
Claudio Marelli <camarelli@gmx.net>
2020-01-12 18:55:12 +0100

    put variables into Recorder class

--- recorder.py
+++ recorder.py
@@ -11,33 +11,32 @@ import os
 from pydub import AudioSegment
 
 
-# audio settings
-dev_index = 2    # device index found by p.get_device_info_by_index(ii)
-chunk     = 1024  # record in chunks of 1024 samples
-channels  = 1     # number of audio channels
-rate      = 16000 # record at 16000 frames per second
-sf        = pyaudio.paInt16 # 16 bits per sample
-
-# auto record settings
-directory = r'./records' # directory to save the files
-threshold = 80 # volume threshold to start recording
-timeout   = 3  # seconds to wait after last threshold
-
-# rms settings
-swidth = 2
-short_normalize = (1.0/32768.0)
-
 class Recorder:
+    # audio settings
+    dev_index = 2    # device index found by p.get_device_info_by_index(ii)
+    chunk     = 1024  # record in chunks of 1024 samples
+    channels  = 1     # number of audio channels
+    rate      = 16000 # record at 16000 frames per second
+    sf        = pyaudio.paInt16 # 16 bits per sample
+
+    # auto record settings
+    directory = r'./records' # directory to save the files
+    threshold = 80 # volume threshold to start recording
+    timeout   = 3  # seconds to wait after last threshold
+
+    # rms settings
+    swidth = 2
+    short_normalize = (1.0/32768.0)
 
     @staticmethod
     def rms(frame):
-        count = len(frame) / swidth
+        count = len(frame) / Recorder.swidth
         format = '%dh' % (count)
         shorts = struct.unpack(format, frame)
 
         sum_squares = 0.0
         for sample in shorts:
-            n = sample * short_normalize
+            n = sample * Recorder.short_normalize
             sum_squares += n * n
         rms = math.pow(sum_squares / count, 0.5)
 
@@ -52,11 +51,11 @@ class Recorder:
         print('Noise detected, recording beginning')
         rec = []
         current = time.time()
-        end = time.time() + timeout
+        end = time.time() + self.timeout
 
         while current <= end:
-            data = self.stream.read(chunk, exception_on_overflow = False)
-            if self.rms(data) >= threshold: end = time.time() + timeout
+            data = self.stream.read(self.chunk, exception_on_overflow = False)
+            if self.rms(data) >= self.threshold: end = time.time() + self.timeout
 
             current = time.time()
             rec.append(data)
@@ -64,16 +63,16 @@ class Recorder:
 
 
     def write(self, recording):
-        if os.path.exists(directory):
-            n_files = len(os.listdir(directory))
-            file_wav = os.path.join(directory, '{}.wav'.format(n_files))
-            file_mp3 = os.path.join(directory, '{}.mp3'.format(n_files))
+        if os.path.exists(self.directory):
+            n_files = len(os.listdir(self.directory))
+            file_wav = os.path.join(self.directory, '{}.wav'.format(n_files))
+            file_mp3 = os.path.join(self.directory, '{}.mp3'.format(n_files))
 
             # creating wave file
             wf = wave.open(file_wav, 'wb')
-            wf.setnchannels(channels)
-            wf.setsampwidth(self.p.get_sample_size(sf))
-            wf.setframerate(rate)
+            wf.setnchannels(self.channels)
+            wf.setsampwidth(self.p.get_sample_size(self.sf))
+            wf.setframerate(self.rate)
             wf.writeframes(recording)
             wf.close()
             print('Written to file: {}'.format(file_wav))
@@ -89,24 +88,24 @@ class Recorder:
             self.bot.callback(file_mp3)
             print('Returning to listening')
         else:
-            print('please create directory to write audio: {}'.format(directory))
+            print('please create directory to write audio: {}'.format(self.directory))
 
 
     def listen(self):
         print('Listening beginning')
         self.listening = True
         self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=sf,
-                                  channels=channels,
-                                  rate=rate,
-                                  input_device_index = dev_index,
+        self.stream = self.p.open(format=self.sf,
+                                  channels=self.channels,
+                                  rate=self.rate,
+                                  input_device_index = self.dev_index,
                                   input=True,
-                                  frames_per_buffer=chunk)
+                                  frames_per_buffer=self.chunk)
 
         while self.listening:
-            input = self.stream.read(chunk, exception_on_overflow = False)
+            input = self.stream.read(self.chunk, exception_on_overflow = False)
             rms_val = self.rms(input)
-            if rms_val > threshold:
+            if rms_val > self.threshold:
                 self.record()
 
         print('Stop listening')
